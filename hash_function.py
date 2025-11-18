f2b7f53ac99a521580dd9ef5397e8ecf96ac6484
Mark Tully <167004432+mtully-64@users.noreply.github.com>
2025-11-18 10:43:19 +0000

    Delete hash_function.py

--- hash_function.py
+++
@@ -1,7 +0,0 @@
-import hashlib
-
-# Now I have to make the function to handle hashing at SHA256
-# This will be in 'hexdigest' not 'digest', so I can read it as a human
-def sha256(data):
-    """Simple SHA256 function"""
-    return hashlib.sha256(data.encode('UTF-8')).hexdigest()
